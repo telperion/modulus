009aa27b1eca64160b689647cfbe165d5883a2f6
telperion <cow4186@rit.edu>
2016-12-11 23:10:42 -0800

    Zedd - "Spectrum (Odyssey's Prism Mix)" [first commit]
    - Draft of SX 12
    - FG change scaffolding (mods HQ, HUD reducer, proxification, etc.)

---
+++ Spectrum (Odyssey's Prism Mix) [Telperion]/spectrum/default.lua
@@ -0,0 +1,351 @@
+-------------------------------------------------------------------------------
+--
+--		Zedd feat. Matthew Koma - "Spectrum (Odyssey's Prism Mix)"
+--		Special Content
+--		
+--		Author: 	Telperion
+--		Date: 		2016-12-11
+--
+--
+--		We'll run where lights won't chase us
+--		Hide where love can save us
+--		I will never let you go
+--
+-------------------------------------------------------------------------------
+
+local sw = SCREEN_WIDTH
+local sh = SCREEN_HEIGHT
+local BPS = GAMESTATE:GetSongBPS()
+local overtime = 0
+local overtimeOffset = 0
+local fgmsg = 0
+local fgcmd = 0
+local plr = {nil, nil}
+
+local DEG_TO_RAD = math.pi / 180.0
+
+--
+-- 		some funktion !
+--
+local SideSign = function(i) return (i == 2) and 1 or -1 end
+
+local RangeScale = function(t, inLower, inUpper, outLower, outUpper)
+	local ti = (t - inLower) / (inUpper - inLower)
+	return outLower + ti * (outUpper - outLower)
+end
+
+-------------------------------------------------------------------------------
+--
+--		Actors begin below this line
+--
+
+
+local Spectrum = Def.ActorFrame {
+	InitCommand = function(self)
+	end,
+	OnCommand = function(self)
+		plr[1] = SCREENMAN:GetTopScreen():GetChild('PlayerP1')
+		plr[2] = SCREENMAN:GetTopScreen():GetChild('PlayerP2')
+		self:sleep(1573)
+	end
+}
+
+
+
+-------------------------------------------------------------------------------
+--
+-- 		Playfield proxies
+--
+
+for pn = 1,2 do
+	Spectrum[#Spectrum + 1] = Def.ActorFrame {	
+		Name = "ProxyP"..pn.."Outer",
+		Def.ActorFrame {	
+			Name = "ProxyP"..pn.."Inner",
+			Def.ActorProxy {					
+				Name = "ProxyP"..pn,
+				InitCommand = function(self)
+					self:aux( tonumber(string.match(self:GetName(), "[0-9]")) )
+				end,
+				BeginCommand=function(self)
+					local McCoy = SCREENMAN:GetTopScreen():GetChild('PlayerP'..self:getaux())
+					if McCoy then 
+						self:SetTarget(McCoy)
+					else 
+						self:hibernate(1573)
+					end
+				end,
+				OnCommand=function(self)
+					local McCoy = SCREENMAN:GetTopScreen():GetChild('PlayerP'..self:getaux())
+					if McCoy then 
+						self:xy(-McCoy:GetX(), -McCoy:GetY())
+						self:GetParent():xy(McCoy:GetX(), McCoy:GetY())
+						Trace("Player "..self:getaux().." Proxy centered itself!")
+					else
+						Trace("Player "..self:getaux().." Proxy couldn't center itself!")
+					end
+				end,
+				RecenterProxyMessageCommand=function(self)					
+					local McCoy = SCREENMAN:GetTopScreen():GetChild('PlayerP'..self:getaux())
+					if McCoy then 
+						self:xy(-McCoy:GetX(), -McCoy:GetY())
+						self:GetParent():xy(McCoy:GetX(), McCoy:GetY())
+					end
+				end,
+			},
+			InitCommand = function(self)
+--				self:aux( tonumber(string.match(self:GetName(), "_([0-9]+)")) )
+			end,
+			CenterProxiesMessageCommand = function(self)
+				self:decelerate(8.0 / BPS):xy(sw/2, sh/2-60)
+			end,
+		},
+		InitCommand = function(self)
+--			self:aux( tonumber(string.match(self:GetName(), "_([0-9]+)")) )
+		end,
+		OnCommand = function(self)
+			self:z(1)
+		end,
+	}
+end
+
+--
+-- Judgment proxies
+--
+for pn = 1,2 do
+	Spectrum[#Spectrum + 1] = Def.ActorProxy {
+		Name = "JudgeP"..pn.."Proxy",
+		InitCommand = function(self)
+			self:aux( tonumber(string.match(self:GetName(), "[0-9]")) )
+		end,
+		BeginCommand = function(self)
+			local McCoy = SCREENMAN:GetTopScreen():GetChild('PlayerP'..self:getaux()):GetChild('Judgment')
+			if McCoy then 
+				self:SetTarget(McCoy)
+				McCoy:visible(false)
+			else 
+				self:hibernate(1573)
+			end
+		end,
+		OnCommand = function(self)
+			local p = self:GetParent()
+						  :GetChild('ProxyP'..self:getaux().."Outer")
+						  :GetChild('ProxyP'..self:getaux().."Inner")
+			
+			self:xy(p:GetX(), sh/2)
+				:zoom(0.8)
+		end,
+	}
+end
+
+--
+-- 		Playfield proxies
+--
+-------------------------------------------------------------------------------
+
+
+
+
+-------------------------------------------------------------------------------
+--
+--		Some ghosting!
+--
+nGhosts = 1
+
+for ghostIndex = 1,nGhosts do
+	local aftMemoryName = "Memory_"..ghostIndex
+	local aftOutputName = "Output_"..ghostIndex
+	local ghostBoyName  = "Ghost_" ..ghostIndex
+	local aftOutSprName = "Sprite_"..ghostIndex
+	
+	local aftMemory = 
+		Def.ActorFrameTexture{
+			Name = aftMemoryName,
+			InitCommand=function(self)
+				self:SetTextureName( self:GetName() )
+					:SetWidth( sw )
+					:SetHeight( sh )
+					:EnableAlphaBuffer( true )
+					:Create()
+			end,
+			Def.Sprite{
+				Name = "Sprite",
+				InitCommand = cmd(Center) 
+			}
+		}
+
+	local aftOutput = 
+		Def.ActorFrameTexture{
+			Name = aftOutputName,
+			InitCommand=function(self)
+				self:SetTextureName( self:GetName() )
+					:SetWidth( sw )
+					:SetHeight( sh )
+					:EnableAlphaBuffer( true )
+					:Create()
+					
+				myMemoryName = "Memory"..string.match(self:GetName(), "Output(_[0-9]+)")
+				Trace(myMemoryName)
+				self:GetParent():GetChild(myMemoryName):GetChild("Sprite"):SetTexture( self:GetTexture() )
+			end,
+			Def.Sprite{	
+				Name = aftOutSprName,
+				Texture = aftMemoryName,
+				InitCommand=function(self)
+				end,
+				BeginCommand=function(self)
+--					local myColorIndex = tonumber(string.match(self:GetName(), "Sprite_([0-9]+)"))
+					self:Center()
+						:zoom(0.998)
+						:diffuse({1,1,1,0.99})
+						:visible(true)
+				end,
+				StopTrailMessageCommand=function(self)
+					self:diffuse({1,1,1,0.0})
+				end,
+				StartTrailMessageCommand=function(self)
+					self:diffuse({1,1,1,0.99})
+				end
+			},
+		}
+	for pn = 1,2 do
+		aftOutput[#aftOutput + 1] = 
+			Def.ActorProxy {					
+				Name = "ProxyP"..pn,
+				BeginCommand=function(self)
+					local pn = string.match(self:GetName(), "ProxyP([12])")
+					local p = self:GetParent()
+								  :GetParent()
+								  :GetChild('ProxyP'..pn.."Outer")
+--								  :GetChild('ProxyP'..pn.."Inner")
+					self:SetTarget(p)
+				end,
+				OnCommand=function(self)
+					self:xy(0, 0)
+				end
+			}
+	end
+		
+	local ghostBoy = 
+		Def.Sprite{
+			Name = ghostBoyName,
+			Texture = aftOutputName,
+			InitCommand=cmd(Center),
+			OnCommand=function(self)
+				local myIndex = tonumber(string.match(self:GetName(), "Ghost_([0-9]+)"))
+				Trace("myIndex: "..myIndex)
+				self:z(3)
+--					:blend("BlendMode_Add")
+					:diffuse({1,1,1,1.0})
+					:visible(true)
+			end,			
+		}
+		
+	Spectrum[#Spectrum + 1] = aftMemory
+	Spectrum[#Spectrum + 1] = aftOutput
+	Spectrum[#Spectrum + 1] = ghostBoy
+end
+
+--
+--		Some ghosting!
+--
+-------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------
+--
+-- 		This is where the shit will be happening.
+--
+
+local messageList = {
+	-- [1]: beat number to issue message on
+	-- [2]: message title
+	-- [3]: optional table of arguments passed to message
+	
+	{  0.00, "CenterProxies"},	
+}
+
+Spectrum[#Spectrum + 1]= Def.Quad {
+	InitCommand = function(self)
+		self:SetHeight(6)
+			:SetWidth(6)
+			:xy(-sw,-sh)
+			:visible(false)
+	end,
+	OnCommand = function(self)
+		self:queuecommand("Update")
+	end,
+	UpdateCommand = function(self)
+		-- Most things are determined by beat, believe it or not.		
+		overtime = GAMESTATE:GetSongBeat() + overtimeOffset
+		
+		-- TODO: this assumes the effect applies over a constant BPM section!!
+		BPS = GAMESTATE:GetSongBPS()
+		
+		-- Hide players and use proxies instead.
+		-- (The FG command system has largely been replaced with the FG messaging system.)
+		if overtime >=   0.0 and fgcmd ==  0 then
+			for i,v in ipairs(plr) do
+				if v then
+					v:visible(false);
+				end
+			end
+			
+			fgcmd = fgcmd + 1
+		end
+				
+		-- Broadcast messages on their own terms.
+		while true do
+			if fgmsg < #messageList then
+				messageBeat, messageName, messageArgs = unpack(messageList[fgmsg+1])
+				if overtime >= messageBeat then			
+					if messageArgs then
+						MESSAGEMAN:Broadcast( messageName, messageArgs )
+					else
+						MESSAGEMAN:Broadcast( messageName )
+					end
+					
+					fgmsg = fgmsg + 1
+				else
+					break;
+				end
+			else
+				break;
+			end
+		end
+				
+		-- Wait a bit and then update again!
+		self:queuecommand("WaitABit")
+	end,
+	WaitABitCommand = function(self)
+		self:sleep(0.02)
+		self:queuecommand("Update")
+	end
+}
+
+
+
+
+-- Load the HUD reducer into this script.
+Spectrum[#Spectrum + 1] = LoadActor("./hudreducer.lua")
+
+-- Load the mods table parser into this script.
+niceSpeed = 420 / 155			-- This song is 155 BPM.
+modsTable = {
+	-- [1]: beat start
+	-- [2]: mod type
+	-- [3]: mod strength (out of unity),
+	-- [4]: mod approach (in beats to complete)
+	-- [5]: player application (1 = P1, 2 = P2, 3 = both, 0 = neither)
+		
+		{   0.0,	"ScrollSpeed",	niceSpeed,    8.0,	3}, 
+		{   0.0,	"Dark",				  0.8,    8.0,	3}, 
+}
+Spectrum[#Spectrum + 1] = LoadActor("./modsHQ.lua", {modsTable, 0})
+
+
+return Spectrum
+
+
+
+
---
+++ Spectrum (Odyssey's Prism Mix) [Telperion]/spectrum/modsHQ.lua
@@ -0,0 +1,234 @@
+-------------------------------------------------------------------------------
+--
+--		Standard Playfield Modifier Headquarters
+--		
+--		Author: 	Telperion
+--		Date: 		2016-12-11
+--
+--
+--		Pass arguments when loading this actor:
+--
+--		local modsTable = {
+--			-- [1]: beat start
+--			-- [2]: mod type
+--			-- [3]: mod strength (out of unity),
+--			-- [4]: mod approach (in beats to complete)
+--			-- [5]: player application (1 = P1, 2 = P2, 3 = both, 0 = neither)
+--		};
+--		local modsLeadBy = 0.009;
+--
+-------------------------------------------------------------------------------
+
+-- Parse arguments.
+modsTable, modsLeadBy = unpack(...)
+
+local modsLaunched = 0;
+
+local modsHQ = Def.ActorFrame {
+	InitCommand = function(self)
+	end,
+	OnCommand = function(self)
+		self:sleep(1573);
+	end
+};
+
+local mods = {
+	["LifeSetting"] =	"ENUM", 
+	["DrainSetting"] =	"ENUM", 
+--	["BatteryLives"] =	"INT", 
+	["TimeSpacing"] =	"FLOAT", 
+	["MaxScrollBPM"] =	"FLOAT", 
+	["ScrollSpeed"] =	"FLOAT", 
+	["ScrollBPM"] =		"FLOAT", 
+	["Boost"] =			"FLOAT", 
+	["Brake"] =			"FLOAT", 
+	["Wave"] =			"FLOAT", 
+	["Expand"] =		"FLOAT", 
+	["Boomerang"] =		"FLOAT", 
+	["Drunk"] =			"FLOAT", 
+	["Dizzy"] =			"FLOAT", 
+	["Confusion"] =		"FLOAT",
+	["Mini"] =			"FLOAT", 
+	["Tiny"] =			"FLOAT", 
+	["Flip"] =			"FLOAT", 
+	["Invert"] =		"FLOAT", 
+	["Tornado"] =		"FLOAT", 
+	["Tipsy"] =			"FLOAT", 
+	["Bumpy"] =			"FLOAT", 
+	["Beat"] =			"FLOAT", 
+	["Xmode"] =			"FLOAT", 
+	["Twirl"] =			"FLOAT", 
+	["Roll"] =			"FLOAT", 
+	["Hidden"] =		"FLOAT", 
+	["HiddenOffset"] =	"FLOAT", 
+	["Sudden"] =		"FLOAT", 
+	["SuddenOffset"] =	"FLOAT", 
+	["Stealth"] =		"FLOAT", 
+	["Blink"] =			"FLOAT", 
+	["RandomVanish"] =	"FLOAT", 
+	["Reverse"] =		"FLOAT", 
+	["Split"] =			"FLOAT", 
+	["Alternate"] =		"FLOAT", 
+	["Cross"] =			"FLOAT", 
+	["Centered"] =		"FLOAT", 
+	["Dark"] =			"FLOAT", 
+	["Blind"] =			"FLOAT", 
+	["Cover"] =			"FLOAT", 
+	["RandAttack"] =	"FLOAT", 
+	["NoAttack"] =		"FLOAT", 
+	["PlayerAutoPlay"] ="FLOAT", 
+	["Skew"] =			"FLOAT", 
+	["Tilt"] =			"FLOAT", 
+	["Passmark"] =		"FLOAT", 
+	["RandomSpeed"] =	"FLOAT", 
+	["TurnNone"] =		"BOOL", 
+	["Mirror"] =		"BOOL", 
+	["Backwards"] =		"BOOL", 
+	["Left"] =			"BOOL", 
+	["Right"] =			"BOOL", 
+	["Shuffle"] =		"BOOL", 
+	["SoftShuffle"] =	"BOOL", 
+	["SuperShuffle"] =	"BOOL", 
+	["NoHolds"] =		"BOOL", 
+	["NoRolls"] =		"BOOL", 
+	["NoMines"] =		"BOOL", 
+	["Little"] =		"BOOL", 
+	["Wide"] =			"BOOL", 
+	["Big"] =			"BOOL", 
+	["Quick"] =			"BOOL", 
+	["BMRize"] =		"BOOL", 
+	["Skippy"] =		"BOOL", 
+	["Mines"] =			"BOOL", 
+	["AttackMines"] =	"BOOL", 
+	["Echo"] =			"BOOL", 
+	["Stomp"] =			"BOOL", 
+	["Planted"] =		"BOOL", 
+	["Floored"] =		"BOOL", 
+	["Twister"] =		"BOOL", 
+	["HoldRolls"] =		"BOOL", 
+	["NoJumps"] =		"BOOL", 
+	["NoHands"] =		"BOOL", 
+	["NoLifts"] =		"BOOL", 
+	["NoFakes"] =		"BOOL", 
+	["NoQuads"] =		"BOOL", 
+	["NoStretch"] =		"BOOL", 
+	["MuteOnError"] =	"BOOL", 
+	["FailSetting"] =	"ENUM", 
+	["MinTNSToHideNotes"] =	"ENUM"
+}
+local clearAllMods = function(playerNum, justTrace)
+	local currValue;
+	local currApproach;
+	
+	playerNum = playerNum or 1
+	justTrace = justTrace or false
+	
+	if playerNum < 1 or playerNum > 2 then do Trace("In clearAllMods: Player number "..playerNum.." is invalid!"); return end end
+	pops = GAMESTATE:GetPlayerState("PlayerNumber_P"..playerNum):GetPlayerOptions("ModsLevel_Song");
+	if pops then
+		for modName,modType in pairs(mods) do
+			currValue,currApproach = pops[modName](pops);
+			-- Trace("In clearAllMods: P"..playerNum.." has mod "..modName.." set to "..tostring(currValue));
+			
+			if not justTrace then
+				if modType == "FLOAT" then
+					if modName == "ScrollBPM" then
+						pops[modName](pops, 200);
+					elseif modName == "ScrollSpeed" then
+						pops[modName](pops, 1);
+					else
+						pops[modName](pops, 0);
+					end
+				elseif modType == "BOOL" then
+					pops[modName](pops, false);
+				elseif modType == "INT" then
+					if modName == "BatteryLives" then
+						pops[modName](pops, 4);
+					else
+						pops[modName](pops, 0);
+					end
+				else -- if modType == "ENUM" then
+				end
+			end
+		end
+	else
+		Trace("In clearAllMods: Player options for "..playerNum.." are not initialized!");
+	end
+end
+
+modsHQ[#modsHQ + 1] = Def.Quad {
+	InitCommand = function(self)
+		self:SetHeight(6)
+			:SetWidth(6)
+			:xy(-12,-12)
+			:visible(false);
+	end,
+	OnCommand = function(self)
+		self:queuecommand("Update");
+		clearAllMods(1);
+		clearAllMods(2);
+	end,
+	UpdateCommand = function(self)
+		-- Most things are determined by beat, believe it or not.		
+		local overtime = GAMESTATE:GetSongBeat();
+		
+		-- TODO: this assumes the effect applies over a constant BPM section!!
+		BPS = GAMESTATE:GetSongBPS()
+		
+		-- Safely initialize the mods table and leading beat count.
+		modsTable 	= modsTable or {};
+		modsLeadBy 	= modsLeadBy or 0;
+		
+		if modsLaunched >= #modsTable then
+			Trace('>>> modsHQ: Hibernated!!');
+			self:hibernate(1573);
+			do return end
+		else
+			while modsLaunched < #modsTable do
+				-- Trace('>>> modsHQ: ' .. modsLaunched);
+				-- Check the next line of the mods table.
+				nextMod = modsTable[modsLaunched + 1];
+				
+				if overtime + modsLeadBy >= nextMod[1] then
+					-- TODO: this assumes the effect applies over a constant BPM section!!
+					Trace('>>> modsHQ: ' .. modsLaunched .. ' @ time = ' .. overtime);
+					
+					for _,pe in pairs(GAMESTATE:GetEnabledPlayers()) do
+						pn = tonumber(string.match(pe, "[0-9]+"));
+						if (nextMod[5] == pn or nextMod[5] == 3) then
+							pops = GAMESTATE:GetPlayerState(pe):GetPlayerOptions("ModsLevel_Song");
+							
+							-- Calculate approach (in units of the value per second):
+							-- a = (value final - value initial) * (beats per second) / (beats for transition + ``machine epsilon``)
+							-- Has to be done individually for each player, just in case they're coming from different initial values :(
+							opVal, opApproach = pops[ nextMod[2] ]( pops );
+							if opApproach == 0 then -- SOMEONE FUCKED UP AND IT WASN'T ME.
+								newApproach = BPS;
+							else
+								newApproach = math.abs(nextMod[3] - opVal) * BPS / (nextMod[4] + 0.001);
+							end
+												pops[ nextMod[2] ]( pops, nextMod[3], newApproach );
+							Trace('>>> modsHQ: ' .. opVal      .. ' @ rate = ' .. opApproach  .. ' for ' .. pe);
+							Trace('>>> modsHQ: ' .. nextMod[3] .. ' @ rate = ' .. newApproach .. ' for ' .. pe .. ' [New!]');
+						end
+					end
+					
+					modsLaunched = modsLaunched + 1;
+				else
+					-- Trace('>>> modsHQ: ' .. overtime .. ' < ' .. nextMod[1]);
+					break;
+				end
+			end
+		end		
+		
+		-- Wait a bit and then update again!
+		self:queuecommand('WaitABit');
+	end,
+	WaitABitCommand = function(self)
+		self:sleep(0.01);
+		self:queuecommand('Update');
+	end
+}
+
+
+return modsHQ;
