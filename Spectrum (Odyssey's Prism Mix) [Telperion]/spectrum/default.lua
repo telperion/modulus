7f6c3b1ede0bf18ca7942c260f99d9f50a291b78
telperion <cow4186@rit.edu>
2016-12-12 23:26:17 -0800

    Zedd - "Spectrum (Odyssey's Prism Mix)" [Lua]
    coming along very nicely. need to AFT the spectral additions to reduce
    on the sheer number of actors but we'll get there soon

--- Spectrum (Odyssey's Prism Mix) [Telperion]/spectrum/default.lua
+++ Spectrum (Odyssey's Prism Mix) [Telperion]/spectrum/default.lua
@@ -22,6 +22,8 @@ local fgmsg = 0
 local fgcmd = 0
 local plr = {nil, nil}
 
+local PI = math.pi
+local LOG2 = math.log(2.0)
 local DEG_TO_RAD = math.pi / 180.0
 
 --
@@ -32,6 +34,13 @@ local SideSign = function(i) return (i == 2) and 1 or -1 end
 local RangeScale = function(t, inLower, inUpper, outLower, outUpper)
 	local ti = (t - inLower) / (inUpper - inLower)
 	return outLower + ti * (outUpper - outLower)
+end
+
+local RangeClamp = function(t, outLower, outUpper)
+	local ti = t
+	ti = (ti < outLower) and outLower or ti
+	ti = (ti > outUpper) and outUpper or ti
+	return ti
 end
 
 -------------------------------------------------------------------------------
@@ -144,13 +153,120 @@ end
 -------------------------------------------------------------------------------
 
 
+-------------------------------------------------------------------------------
+--
+--		Extra nuts 'n' bolts
+--
+
+local texArrows = {
+	0.000,		-- 4th
+	0.375,		-- 16th
+	0.125,		-- 8th
+	0.250,		-- 12th
+	0.625,		-- 32nd
+	0.875,		-- 64th
+--	0.500,		-- 24th
+--	0.750,		-- 48th
+	}			-- Texture coordinate shifts
+
+	
+for quantIndex,quantTexCoord in ipairs(texArrows) do
+	Spectrum[#Spectrum + 1] = 
+		NOTESKIN:LoadActorForNoteSkin("Down", "Tap Note", "cyber") .. {
+			Name = "SpectrumNote"..quantIndex,
+			InitCommand = function(self)
+				self:visible(false)
+					:xy(0, 0)
+					:texturetranslate(quantTexCoord, 0)
+--				Trace("Created the source note "..quantIndex.."!")
+			end,
+		}
+end
+
+
+local SpectralAdditions =
+	Def.ActorFrameTexture {
+		Name = "SpectralAdditions",
+		InitCommand = function(self)
+			self:SetTextureName( self:GetName() )
+				:SetWidth(sw)
+				:SetHeight(sw)
+				:EnableAlphaBuffer( true )
+				:Create()
+		end,
+	}
+
+
+-- The Wheel of Fate is Turning.. . .. . .
+for i,_ in ipairs(texArrows) do
+	local WheelOfFate = 
+		Def.ActorFrame {
+			Name = "WheelOfFate"..i,
+			InitCommand = function(self)
+				self:aux( 1 )
+				self:visible(false)
+			end,
+			OnCommand = function(self)
+				self:xy(0, 0)
+					:queuecommand("Rotater")
+			end,
+			RotaterCommand = function(self)
+				self:aux( self:getaux() * -1 )
+				self:smooth((7.0 + math.random() * 4.0) / BPS)
+					:rotationz(180 * math.random() * self:getaux())
+					:queuecommand("Rotater")
+			end,
+		}
+	
+	local spokesCount = 3 + 2 * i
+	local bestRadius = 64 * 2.5 * (spokesCount - 3) / (2*PI)
+	for j = 1,spokesCount do
+		WheelOfFate[#WheelOfFate + 1] = 
+			Def.ActorProxy {
+				Name = "NoteProxy"..j,	
+				InitCommand = function(self)
+					self:aux( i )
+				end,	
+				BeginCommand=function(self)
+					local McCoy = self:GetParent()
+									  :GetParent()
+									  :GetChild("SpectrumNote"..self:getaux())
+					if McCoy then 
+						self:SetTarget(McCoy)
+							:zoom(1 - self:getaux()*0.05)
+							:visible(true)
+					else 
+						self:hibernate(1573)
+					end
+				end,
+				OnCommand=function(self)
+					local myIndex = tonumber(string.match(self:GetName(), "[0-9]+"))
+					self:xy(bestRadius * math.cos(2*PI * j/spokesCount), bestRadius * math.sin(2*PI * j/spokesCount))
+						:rotationz(360 * j/spokesCount + 90)
+				end,
+			}
+	end
+		
+	Spectrum[#Spectrum + 1] = WheelOfFate
+end
+	
+--
+-- 		Extra nuts 'n' bolts
+--
+-------------------------------------------------------------------------------
+
 
 
 -------------------------------------------------------------------------------
 --
 --		Some ghosting!
 --
-nGhosts = 1
+nGhosts = 3
+colorGhosts = {
+	{1, 1, 0, 1},
+	{0, 1, 1, 1},
+	{1, 0, 1, 1},
+	}
 
 
 --
@@ -158,15 +274,13 @@ nGhosts = 1
 --		try slapping it on a distorted ActorMultiVertex instead.
 --		Setting up the vertex coordinates for that takes a little bit of work.
 --
-local PI    = math.pi;
-local LOG2  = math.log(2.0);
-local spectralRows = 36;			-- assuming 16:9
-local spectralCols = 64;			-- assuming 16:9
-local spectralDX = sw/spectralCols;
-local spectralDY = sh/spectralRows;
+local spectralRows = 36				-- assuming 16:9
+local spectralCols = 64				-- assuming 16:9
+local spectralDX = sw/spectralCols
+local spectralDY = sh/spectralRows
 
-local tw = math.exp(math.ceil(math.log(sw)/LOG2) * LOG2);
-local th = math.exp(math.ceil(math.log(sh)/LOG2) * LOG2);
+local tw = math.exp(math.ceil(math.log(sw)/LOG2) * LOG2)
+local th = math.exp(math.ceil(math.log(sh)/LOG2) * LOG2)
 
 local CalculateRowBaseVertices = function(rowIndex)
 	verts = {}
@@ -203,7 +317,14 @@ local CalculateBaseTextures = function(verts)
 	return verts	
 end
 
-local CalculateShiftingTextures = function(verts, gradient)
+local Gradient_NullShift = function(x, y, z)
+	return 0, 0
+end
+
+local CalculateShiftingTextures = function(verts, gradient, soul)
+	gradient = gradient or Gradient_NullShift
+	soul = soul or 0
+	
 	-- Coordinate the texture all at once.
 	for vertIndex = 1,#verts do
 		local x = verts[vertIndex][1][1] / sw - 0.5
@@ -211,26 +332,24 @@ local CalculateShiftingTextures = function(verts, gradient)
 		
 		-- The gradient function operates on [-1, 1] × [-1, 1].
 		-- The magnitude of the gradient is expected to be scaled to that same space.
-		xn, yn = gradient(2*x, 2*y)
+		-- The third parameter is used to differentiate between multiple AMV ghosts.
+		xn, yn = gradient(2*x, 2*y, soul)
 		
 		-- Apply the gradient onto the vertices after scaling to texture space.
-		verts[vertIndex][3][1] = (verts[vertIndex][1][1] + xn * sw)/tw
-		verts[vertIndex][3][2] = (verts[vertIndex][1][2] + yn * sh)/th
+		verts[vertIndex][3][1] = RangeClamp(verts[vertIndex][1][1] + xn * sw, 0, sw-1)/tw
+		verts[vertIndex][3][2] = RangeClamp(verts[vertIndex][1][2] + yn * sh, 0, sh-1)/th
 	end
 
 	return verts	
 end
 
 
-local Gradient_NullShift = function(x, y)
-	return 0, 0
-end
 
-local Gradient_HorizontalSpread = function(x, y)
+local Gradient_HorizontalSpread = function(x, y, z)
 	local T = 1.8				-- Side crest distance from center
-	local M = 0.3				-- Scaling factor for X direction
-	local N = 0.8				-- Scaling factor for Y direction
-	local A = 0.01				-- Maximum mplitude of shift
+	local M = 0.7 + z			-- Scaling factor for X direction
+	local N = 0.8 + z			-- Scaling factor for Y direction
+	local A = 0.002				-- Maximum amplitude of shift
 	
 	local Trad = 2*PI/T
 	local B = 1 + M*x*x + N*y*y
@@ -242,29 +361,53 @@ local Gradient_HorizontalSpread = function(x, y)
 	return xn, yn
 end
 
+local Gradient_Sinkholes = function(x, y, z)
+	local ph0 =  -PI/2			-- Angle of first sinkhole
+	local phi = 2*PI/7			-- Additional angle per sinkhole
+	local R = 1.1				-- Radial distance of sinkholes from center
+	local M = 2.0 + z			-- Scaling factor for X direction
+	local N = 2.0 - z			-- Scaling factor for Y direction
+	local A = 0.002				-- Maximum amplitude of shift
+	
+	local xn = 0
+	local yn = 0
+	
+	-- The function is additive, so calculate each sinkhole's contribution individually.
+	for sinkhole = 1,7 do
+		local theta = ph0 + sinkhole * phi
+		local xshift = x - R*math.cos(theta)
+		local yshift = y - R*math.sin(theta)
+		
+		local B = 1 + M*xshift*xshift + N*yshift*yshift
+		local BB = B*B
+			
+		xn = xn + 2*A*M*xshift/BB
+		yn = yn + 2*A*N*yshift/BB
+	end
+	
+	-- Unsinkhole the center.	
+	local B = 1 + M*x*x + N*y*y
+	local BB = B*B
+		
+	xn = xn - 2*A*M*x/BB
+	yn = yn - 2*A*N*y/BB
+	
+	return xn, yn
+end
 
 
-local spectralAMV = 
-	Def.ActorFrame {
-		Name = "SpectralAMV"
-	}
-
-for rowIndex = 1,spectralRows do
-	spectralAMV[#spectralAMV + 1] =
-		Def.ActorMultiVertex {
-			Name = "SpectralAMVStrip_"..rowIndex,
-			InitCommand = function(self)
-				local verts = CalculateRowBaseVertices(rowIndex)
-				-- verts = CalculateBaseTextures(verts)
-				-- verts = CalculateShiftingTextures(verts, Gradient_NullShift)
-				verts = CalculateShiftingTextures(verts, Gradient_NullShift)
-				self:xy(0, 0)
-					:SetVertices(verts)
-					:SetDrawState{First = 1,
-								  Num = spectralCols * 2,
-								  Mode = "DrawMode_QuadStrip"}
-			end,
-		}
+local Gradient_Drip = function(x, y, z)
+	local T = 0.4				-- Period of drip wave
+	local M = 0.3 + 0.1 * z		-- Scaling factor for X direction
+	local N = 1.0 + z			-- Scaling factor for Y direction
+	local A = 0.002				-- Maximum amplitude of shift
+	
+	local Trad = 2*PI/T
+		
+	local xn = math.sin(Trad * y) * A*M*Trad
+	local yn = -A*N*(y+1)
+	
+	return xn, yn
 end
 
 
@@ -273,6 +416,31 @@ for ghostIndex = 1,nGhosts do
 	local aftOutputName = "Output_"..ghostIndex
 	local ghostBoyName  = "Ghost_" ..ghostIndex
 	local aftOutSprName = "Sprite_"..ghostIndex
+
+
+
+	local spectralAMV = 
+		Def.ActorFrame {
+			Name = "SpectralAMV"
+		}
+
+	for rowIndex = 1,spectralRows do
+		spectralAMV[#spectralAMV + 1] =
+			Def.ActorMultiVertex {
+				Name = "SpectralAMVStrip_"..rowIndex,
+				InitCommand = function(self)
+					local verts = CalculateRowBaseVertices(rowIndex)
+					-- verts = CalculateBaseTextures(verts)
+					-- verts = CalculateShiftingTextures(verts, Gradient_NullShift)
+					verts = CalculateShiftingTextures(verts, Gradient_Drip, (ghostIndex-2)*0.5)
+					self:xy(0, 0)
+						:SetVertices(verts)
+						:SetDrawState{First = 1,
+									  Num = (spectralCols + 1) * 2,
+									  Mode = "DrawMode_QuadStrip"}
+				end,
+			}
+	end
 	
 	local aftMemory = 
 		Def.ActorFrameTexture{
@@ -311,18 +479,19 @@ for ghostIndex = 1,nGhosts do
 				Name = aftOutSprName,
 				Texture = aftMemoryName,
 				InitCommand=function(self)
+					self:aux( tonumber(string.match(self:GetName(), "Sprite_([0-9]+)")) )
 				end,
 				BeginCommand=function(self)
---					local myColorIndex = tonumber(string.match(self:GetName(), "Sprite_([0-9]+)"))
 					self:Center()
-						:diffuse({1,1,1,0.99})
+						:diffuse(colorGhosts[self:getaux()])
+						:diffusealpha(0.998)
 						:visible(true)
 				end,
 				StopTrailMessageCommand=function(self)
-					self:diffuse({1,1,1,0.0})
+					self:diffusealpha(0.0)
 				end,
 				StartTrailMessageCommand=function(self)
-					self:diffuse({1,1,1,0.99})
+					self:diffusealpha(0.998)
 				end
 			},
 		}
@@ -343,6 +512,36 @@ for ghostIndex = 1,nGhosts do
 				end
 			}
 	end
+	for quant = 1,#texArrows do
+		aftOutput[#aftOutput + 1] = 
+			Def.ActorProxy {					
+				Name = "ProxyWheelOfFate"..quant,
+				BeginCommand=function(self)
+					local myIndex = string.match(self:GetName(), "ProxyWheelOfFate([0-9])")
+					local wof = self:GetParent()
+									:GetParent()
+									:GetChild("WheelOfFate"..myIndex)
+					self:SetTarget(wof)
+				end,
+				OnCommand=function(self)
+					self:xy(0, sh/2)
+				end
+			}
+		aftOutput[#aftOutput + 1] = 
+			Def.ActorProxy {					
+				Name = "ProxyWheelOfFate"..quant,
+				BeginCommand=function(self)
+					local myIndex = string.match(self:GetName(), "ProxyWheelOfFate([0-9])")
+					local wof = self:GetParent()
+									:GetParent()
+									:GetChild("WheelOfFate"..myIndex)
+					self:SetTarget(wof)
+				end,
+				OnCommand=function(self)
+					self:xy(sw, sh/2)
+				end
+			}
+	end
 		
 	local ghostBoy = 
 		Def.Sprite{
@@ -352,9 +551,9 @@ for ghostIndex = 1,nGhosts do
 			OnCommand=function(self)
 				local myIndex = tonumber(string.match(self:GetName(), "Ghost_([0-9]+)"))
 				Trace("myIndex: "..myIndex)
-				self:z(3)
+				self:z(0.5)
 					:blend("BlendMode_Add")
-					:diffuse({1,1,1,0.7})
+					:diffuse({1,1,1,0.4})
 					:visible(true)
 			end,			
 		}
